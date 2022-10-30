1cbad140c2e2d6f843bc7698b7170ef004db1fc8
Britney Hemmings <britneyhemmings1@gmail.com>
2022-10-30 14:53:34 -0500

    Edited 3 scripts.js

--- scripts.js
+++ scripts.js
@@ -5,14 +5,18 @@ window.addEventListener('DOMContentLoaded', function (){
     var message = document.getElementsByClassName('message')[0];
     console.log(message);
 
+    function isValidEmail(emailAddress) {
+        return /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/.test(emailAddress);
+      }
+
     submitBtn.addEventListener("click", e => {
         e.preventDefault();
         textField = document.getElementById("email");
         var email = textField.value;
         console.log(email);
-        if(email == ''){
+        if(email == '' || !isValidEmail(email)){
             message.innerHTML = "Please enter a valid email address."
-        } else{
+        } else if (isValidEmail(email)){
         message.innerHTML = `Thank You! Your email address ${email} has been added to our mailing list!`;
         }
         textField.value = "";
